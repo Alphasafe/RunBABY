53188d3326dc5f8bc8bf3ad4a7448209ec15c7cc
Alphasafe <btuguldur91@yahoo.com>
2017-05-02 11:10:38 +0900

    Add files via upload

---
+++ Map.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Runbaby
+{
+    class Map
+    {
+        public static int c = 2;
+        public static void drawBoard(){
+            const char toMap = 'T';
+            const char cor1 = '-';
+            const char cor2 = '|';
+            for ( int i = 1; i < 100;i++)
+            {
+                if (i != c)
+                {
+                    Console.SetCursorPosition(i, 10);
+                    Console.Write(toMap);
+                }
+                else
+                {
+                    Console.SetCursorPosition(i, 10);
+                    Console.Write(' ');
+                }
+            }
+            Console.SetCursorPosition(0, 0);
+            for (int i = 1; i < 102; i++)
+            {
+                Console.Write(cor1);
+            }
+            Console.SetCursorPosition(1,11);
+            for (int i = 1; i < 101; i++)
+            {
+                Console.Write(cor1);
+            }
+            for (int i = 0; i < 12; i++)
+            {
+                Console.SetCursorPosition(0, i);
+                Console.Write(cor2);
+            }
+            for (int i = 0; i < 12; i++)
+            {
+                Console.SetCursorPosition(101, i);
+                Console.Write(cor2);
+            }
+        }
+    }
+}
