adf3cb502c2979b4a5b452ec30313fa9c28b9fdf
Alphasafe <btuguldur91@yahoo.com>
2017-05-02 11:11:14 +0900

    Add files via upload
    Main bolon runner tsug oruulsan

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,16 +10,12 @@ namespace Runbaby
     {
         public static void Main(string[] args)
         {
-            const char toWrite = '*'; // Character to write on-screen.
+            const char toWrite ='*';
 
-            int x = 5, y = 8; // Contains current cursor position.
-
-            Write(toWrite); // Write the character on the default location (0,0).
-            Console.SetCursorPosition(x, y);
-            for (int i = 10; i < 70; i++)
-            {
-                Console.Write("-");
-            }
+            int x = 1, y = 9;
+            int x1= 5, y1= 5;
+            Write(toWrite);
+            Map.drawBoard();
             while (true)
             {
                 if (Console.KeyAvailable)
@@ -29,33 +25,54 @@ namespace Runbaby
                     switch (command)
                     {
                         case ConsoleKey.DownArrow:
-                            y++;
-                            break;
-                        case ConsoleKey.UpArrow:
-                            if (y > 0)
+                            if(y < 9)
                             {
-                                y--;
+                                y++;
                             }
                             break;
+                        case ConsoleKey.UpArrow:
+                            if (y > 2)
+                            {
+                                y -= 2;
+                            }  
+                            break;
                     }
-
-                    Write(toWrite, x, y);
+                }
+                if (y < 9)
+                {
+                    y++;
+                }
+                if (x < 15)
+                {
+                    x++;
+                }
+                Console.SetCursorPosition(x1, y1);
+                Console.Write(" ");
+                Console.SetCursorPosition(x, y);
+                Write(toWrite, x, y);
+                x1 = x;
+                y1 = y;
+                if (Map.c > 0)
+                {
+                    Map.c--;
                 }
                 else
                 {
-                    Thread.Sleep(100);
+                    Map.c = 99;
                 }
+                Map.drawBoard();
+                Thread.Sleep(100);
             }
         }
-
-        public static void Write(char toWrite, int x = 0, int y = 0)
+        
+        public static void Write(char toWrite, int x = 5, int y = 5)
         {
             try
             {
                 if (x >= 0 && y >= 0) // 0-based
                 {
-                    Console.Clear();
                     Console.SetCursorPosition(x, y);
+                    Console.CursorVisible = false;
                     Console.Write(toWrite);
                 }
             }
