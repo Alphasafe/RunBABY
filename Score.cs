7ffc90fc247b3a1a71a84fdb7d14cf1ec2f310b7
Alphasafe <btuguldur91@yahoo.com>
2017-05-18 14:07:47 +0900

    Add files via upload

---
+++ Score.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Runbaby
+{
+    class Score
+    {
+        public int score;
+        public Score(int score){
+            this.score = score;
+        }
+        public void plus()
+        {
+            if (score < 100)
+            {
+                score += 1;
+            }
+            else if(score > 100){
+                score += 1;
+            }
+            else if (score > 1000)
+            {
+                score += 5;
+            }
+            else
+            {
+                score += 10;
+            }
+        }
+        public int get()
+        {
+            return score;
+        }
+    }
+}
